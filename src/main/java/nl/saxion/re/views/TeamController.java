3707d7abc92e7e0ea93fca397c010f712c5bd692
Timo Brouwer <timobrouwer2@gmail.com>
2023-04-06 22:31:01 +0200

    fixed installer page

--- src/main/java/nl/saxion/re/views/TeamController.java
+++ src/main/java/nl/saxion/re/views/TeamController.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
